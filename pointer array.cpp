8715df4eea2cc146e993556df065bb39b85dbbe8
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-14 18:25:54 -0500

    with array
    arrays have built in pointers

--- pointer array.cpp
+++ pointer array.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main(){
-    
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10}; 
+
+    int * arr2 = arr; //array is just an int pointer
+    cout << *arr << endl;
+
+
+    int* ptr = &arr[0];
+
+    cout << ptr << endl;
+    cout << *ptr << endl;
+
+    cout << *(ptr+1)<< endl;
+// both top and botom are equivalent //
+    cout << arr[1] << endl;
+
     return 0;
 }
